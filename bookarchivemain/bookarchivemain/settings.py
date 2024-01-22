6a1375e5feb8dbc6a8ec47dfd28f045e2ce5efa4
Comp22023 <comp22023@mail.ru>
2024-01-22 21:51:59 +0300

    ADDED img in news_block

--- bookarchivemain/bookarchivemain/settings.py
+++ bookarchivemain/bookarchivemain/settings.py
@@ -127,8 +127,8 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "bookapp/static"),
 ]
 
-MEDIA_URL = ''
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img/')
+MEDIA_URL = 'img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'bookapp/static/img')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
