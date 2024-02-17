8f24f6daaf2e79a54b1bbea0a525f4f254e5de62
Comp22023 <143874075+Comp22023@users.noreply.github.com>
2024-02-17 15:14:57 +0300

    Update settings.py

--- bookarchivemain/bookarchivemain/settings.py
+++ bookarchivemain/bookarchivemain/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'bookarchivemain.wsgi.application'
 DATABASES = {
         'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://writer:ujCvRJOoVVQHduyQCs5W3YqKUHgo8Wuf@localhost:5432/bookdb_woaj',
+        default='postgres://writer:ujCvRJOoVVQHduyQCs5W3YqKUHgo8Wuf@dpg-cn892af109ks739n97d0-a.frankfurt-postgres.render.com/bookdb_woaj',
         conn_max_age=600
     )
 }
