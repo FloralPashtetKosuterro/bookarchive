77959fd6b491369f8a6cb05dc915c4fa3a07d3b8
FloralPashtetKosuterro <hahatun2121@gmail.com>
2024-02-20 23:46:20 +0300

    adaptation pt1

--- bookarchivemain/bookarchivemain/urls.py
+++ bookarchivemain/bookarchivemain/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from bookapp.views import page_not_found
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('bookapp.urls')),
 ]
 
